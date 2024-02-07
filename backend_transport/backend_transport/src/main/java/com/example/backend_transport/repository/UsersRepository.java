61501b4688e9afc392d9d26bb9a420009236056f
lovasoa3 <hei.lovasoa.3@gmail.com>
2024-02-07 10:08:28 +0100

    feat:ajouter users repository

--- backend_transport/backend_transport/src/main/java/com/example/backend_transport/repository/UsersRepository.java
+++ backend_transport/backend_transport/src/main/java/com/example/backend_transport/repository/UsersRepository.java
@@ -4,9 +4,6 @@ import com.example.backend_transport.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Integer> {
-    Users findUser(String name,String email,String gender,String numero,String cin,String adresse);
 }
