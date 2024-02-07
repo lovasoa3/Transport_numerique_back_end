d24edeb91aa53b6b0400c8e0cb4fbd3ce3057793
lovasoa3 <hei.lovasoa.3@gmail.com>
2024-02-07 10:12:16 +0100

    feat:ajouter  users service

--- backend_transport/backend_transport/src/main/java/com/example/backend_transport/service/UserService.java
+++ backend_transport/backend_transport/src/main/java/com/example/backend_transport/service/UserService.java
@@ -2,31 +2,25 @@ package com.example.backend_transport.service;
 
 import com.example.backend_transport.model.Users;
 import com.example.backend_transport.repository.UsersRepository;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.lang.Iterable;
+
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class UserService {
-    @Autowired
-    private final UsersRepository repository;
+     @Autowired
+    private UsersRepository repository;
 
-    public UserService(UsersRepository repository) {
-        this.repository = repository;
-    }
+    public void registreUser(Users users) {
+        repository.save(users);
 
-    public List<Users> getAllUser(){
-        return repository.findAll();
     }
-    public Optional<Users> getOneUser(int id){
+    public List<Users> listUsers(){
+       return repository.findAll();
+    }
+    public Optional<Users> oneUser(Integer id){
         return repository.findById(id);
     }
-    @Transactional
-    public List<Users> saveAlls(Users user){
-        return (List<Users>) repository.save(user);
-    }
-
 }
